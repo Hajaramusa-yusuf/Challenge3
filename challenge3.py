7d5e5ac9331305bb40b2099d2bc2687b5c136fe8
Hajaramusa-yusuf <83853748+Hajaramusa-yusuf@users.noreply.github.com>
2021-05-09 04:18:46 +0100

    Update challenge3.py

--- challenge3.py
+++ challenge3.py
@@ -2,6 +2,10 @@ import random
 
 hat = [1,2,4,8,16,32,64]
 
+# since k = 1, 2, ..., 7.
+# we will initilaze all the numbers in the list to use 2^k-1 staring from k=1 to k=7 
+#this will give us hat = [1,2,4,8,16,32,64]
+
 sum_check = 124
 probable_sum = 0
 
